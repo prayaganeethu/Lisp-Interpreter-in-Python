c965e844a672b80ccf0d6c68ddd5824328dd6a6b
Neethu Mohandas <prayaganeethu@gmail.com>
2017-05-13 21:24:04 +0530

    Lispy Calculator in Python - a subset of Scheme

--- lispIn.py
+++ lispIn.py
@@ -1,7 +1,12 @@
-env = {}
+import math
+import operator as op
 
-mathematical_operators = {"+", "-", "*", "/"}
+mathematical_operators = {"+", "-", "*", "/", "rem"}
 relational_operators = {">", "<", "<=", ">=", "/=", "="}
+
+Symbol = str
+List = list
+Number = (int, float)
 
 
 def tokenize_lisp_prog(lisp_program):
@@ -38,8 +43,17 @@ def parse_lisp_program(lisp_program):
     return read_as_list(tokenize_lisp_prog(lisp_program))
 
 
-def quote(a):
-    return tuple(a)
+def eq(oprnds):
+    return oprnds[0] == oprnds[1]
+
+
+def quote(oprnds):
+    return tuple(oprnds)
+
+
+def define(oprnds):
+    env[oprnds[0]] = oprnds[1]
+    # print("DEFINE : {}".format(env[oprnds[0]]))
 
 
 def math_operators(opr_oprnds):
@@ -52,93 +66,126 @@ def math_operators(opr_oprnds):
         product = 1
         for term in opr_oprnds[1:]:
             product = product * eval_lisp_program(term)
-        return product
+        return float(product)
+    elif opr_oprnds[0] == "rem":
+        return eval_lisp_program(opr_oprnds[1]) % eval_lisp_program(opr_oprnds[2])
     elif opr_oprnds[0] == "-":
         return eval_lisp_program(opr_oprnds[1]) - eval_lisp_program(opr_oprnds[2])
     elif opr_oprnds[0] == "/":
         return eval_lisp_program(opr_oprnds[1]) // eval_lisp_program(opr_oprnds[2])
 
 
-def rel_operators(x):
-    if x[0] == "/=":
-        return eval_lisp_program(x[1]) != eval_lisp_program(x[2])
-    elif x[0] == "=":
-        return eval_lisp_program(x[1]) == eval_lisp_program(x[2])
+def rel_operators(opr_oprnds):
+    if opr_oprnds[0] == "/=":
+        return eval_lisp_program(opr_oprnds[1]) != eval_lisp_program(opr_oprnds[2])
+    elif opr_oprnds[0] == "=":
+        return eval_lisp_program(opr_oprnds[1]) == eval_lisp_program(opr_oprnds[2])
     else:
-        return eval(str(eval_lisp_program(x[1])) + " " + str(x[0]) + " " + str(eval_lisp_program(x[2])))
+        return eval(str(eval_lisp_program(opr_oprnds[1])) + " " +
+                    str(opr_oprnds[0]) + " " + str(eval_lisp_program(opr_oprnds[2])))
 
 
-def if_clause(x):
-    if eval_lisp_program(x[0]) == True:
-        return eval_lisp_program(x[1])
+def if_clause(oprnds):
+    print("IF {} {} {}".format(oprnds[0], oprnds[1], oprnds[2]))
+    if eval_lisp_program(oprnds[0]) == True:
+        return eval_lisp_program(oprnds[1])
     else:
-        return eval_lisp_program(x[2])
+        return eval_lisp_program(oprnds[2])
 
 
-def cons(a, b):
-    if a == ["nil"]:
-        return b
-    elif b == ["nil"]:
-        return a
+def car(oprnds):
+    return oprnds[0][0]
+
+
+def cdr(oprnds):
+    return oprnds[0][1:]
+
+
+def cons(oprnds):
+    if oprnds[0] == ["nil"]:
+        return oprnds[1]
+    elif oprnds[1] == ["nil"]:
+        return oprnds[0]
     else:
-        return a + b
+        return oprnds[0] + oprnds[1]
 
 
-def max(a):
-    big = eval_lisp_program(a[0])
-    b = a[1:]
-    for term in b:
-        if eval_lisp_program(term) > big:
-            big = eval_lisp_program(term)
-    return big
+def update(oprnds):
+    env[oprnds[0]] = oprnds[1]
 
 
-def min(a):
-    small = eval_lisp_program(a[0])
-    b = a[1:]
-    for term in b:
-        if eval_lisp_program(term) < small:
-            small = eval_lisp_program(term)
-    return small
+def begin(*oprnds):
+    return oprnds[-1]
 
 
-def update(a, b):
-    env[a] = b
+def list(oprnds):
+    return list(oprnds)
+
+
+def is_list(oprnds):
+    return isinstance(oprnds, list)
+
+
+def is_null(oprnds):
+    return oprnds == []
+
+
+def is_number(oprnds):
+    return isinstance(oprnds, Number)
+
+
+def is_symbol(oprnds):
+    return isinstance(oprnds, Symbol)
+
+
+env = {"eq?": op.is_,
+       "equal?": op.eq,
+       "quote": quote,
+       "car": car,
+       "cdr": cdr,
+       "cons": cons,
+       "max": max,
+       "min": min,
+       "update": update,
+       "abs": abs,
+       "length": len,
+       "map": map,
+       "round": round,
+       "not": op.not_,
+       "procedure?": callable,
+       "set!": update,
+       "begin": begin,
+       "list": list,
+       "list?": is_list,
+       "null?": is_null,
+       "number?": is_number,
+       "symbol?": is_symbol
+       }
+
+env.update(vars(math))
 
 
 def eval_lisp_program(x):
-    if isinstance(x, list):
-        if x[0] == "eq?":
-            return eq(eval_lisp_program(x[1]), eval_lisp_program(x[2]))
-        if x[0] == "quote":
-            return quote(eval_lisp_program(x[1:]))
-        if x[0] == "define":
-            env[x[1]] = eval_lisp_program(x[2])
-            print(env)
-        if x[0] == "if":
-            return if_clause(x[1:])
-        if x[0] in mathematical_operators:
-            return math_operators(x)
-        if x[0] in relational_operators:
-            return rel_operators(x)
-        if x[0] == "car":
-            return x[1][0]
-        if x[0] == "cdr":
-            return x[1][1:]
-        if x[0] == "cons":
-            return cons([x[1]], [x[2]])
-        if x[0] == "max":
-            return max(x[1:])
-        if x[0] == "min":
-            return min(x[1:])
-        if x[0] == "update":
-            return update(x[1], eval_lisp_program(x[2]))
-        if x[0] == "#":
-            pass
-    elif isinstance(x, str):
+    global env
+    if isinstance(x, Symbol):
         return env[x]
-    elif isinstance(x, int):
+    elif isinstance(x, Number):
         return x
+    elif x[0] == "define":
+        return define(x[1:])
+    elif x[0] == "if":
+        return if_clause(x[1:])
+    elif x[0] in mathematical_operators:
+        # print(x[0])
+        return math_operators(x)
+    elif x[0] in relational_operators:
+        return rel_operators(x)
+    elif x[0] == "#":
+        pass
+    else:
+        args = [eval_lisp_program(arg) for arg in x[1:]]
+        # print(args)
+        return env[x[0]](*args)
 
 program = input("Enter Lisp Input: ")
 parsed_list = parse_lisp_program(program)
